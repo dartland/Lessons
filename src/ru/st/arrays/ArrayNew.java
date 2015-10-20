989ffd693c923f5e5837606a99bdd48cffa80fb7
dartland <dartland@rambler.ru>
2015-10-20 23:42:40 +0300

    arrays

---
+++ src/ru/st/arrays/ArrayNew.java
@@ -0,0 +1,18 @@
+package ru.st.arrays;
+//: initialization/ArrayNew.java
+// Создание массивов оператором new.
+import java.util.*;
+import static ru.st.print.Print.*;
+ 
+public class ArrayNew {
+  public static void main(String[] args) {
+    int[] a;
+    Random rand = new Random(47);
+    a = new int[rand.nextInt(20)];
+    print("length of a = " + a.length);
+    print(Arrays.toString(a));
+    Arrays.asList(a);
+    String[] string = "My dog has fleas".split(" ");
+    print(string[1]);
+  }
+}
